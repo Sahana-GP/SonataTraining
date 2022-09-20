02bb05649cb9db91a8d3d13a1d4a164686662a2f
SAHANA P <74894872+Sahana-GP@users.noreply.github.com>
2022-09-20 17:57:43 +0530

    Add files via upload

--- SpringMVC/src/main/java/com/sonata/controller/TopicController.java
+++ SpringMVC/src/main/java/com/sonata/controller/TopicController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sonata.model.Topic;
@@ -37,4 +39,19 @@ public class TopicController {
 		return topicService.getSingleTopic(id);
 	}
 	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+public void UpdateTopic(@RequestBody Topic topic, @PathVariable String id) {
+	topicService.updateTopic(id, topic);
+}
+
+@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+public void DeleteTopic(@RequestBody Topic topic, @PathVariable String id) {
+	topicService.deleteTopic(id, topic);
+}
+
 }
--- SpringMVC/src/main/java/com/sonata/controller/TopicService.java
+++ SpringMVC/src/main/java/com/sonata/controller/TopicService.java
@@ -25,4 +25,22 @@ public class TopicService {
 		return topics.stream().filter(t-> t.getId().equals(id)).findFirst().get();
 	}
 	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
+	public void updateTopic(String id, Topic topic) {
+		for(int i = 0; i<topics.size(); i++) {
+		Topic t = topics.get(i);
+		if(t.getId().equals(id)) {
+			topics.set(i, topic);
+			return;
+		}
+		}
+		}
+	
+	public void deleteTopic(String id, Topic topic) {
+		topics.removeIf(t->t.getId().equals(id));
+	}
+	
 }
